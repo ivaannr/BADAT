03bf49bb48547455647ae9ca1ca126befa1403ea
iván <128229332+ivaannr@users.noreply.github.com>
2025-05-27 13:41:03 +0200

    Update ejercicioArturo.sql

--- ejercicioArturo.sql
+++ ejercicioArturo.sql
@@ -8,7 +8,7 @@ use [CASAS RURALES]
 --Crear un trigger en el que el sistema tome el control a la hora de actualizar
 --las Habitaciones y tenga en cuenta que el precio de las habitaciones sin baño
 --no pueden superar el precio de las que tienen baño del mismo alojamiento
---Crear un procedimiento temporal que suba el precio de todas las habitaciones sin baño
+--Crear un procedimiento temporal que suba el precio 5 pavines de todas las habitaciones sin baño
 --que organicen una actividad de dificultad 4
 
 go
@@ -62,13 +62,14 @@ end try begin catch
 end catch
 END
 
-create function fn_maxPrecio(@habitacion int, @alojamiento int)
-returns int
+create function fn_comparePrecio(@alojamiento int, @precio decimal(5,2))
+returns bit
 as
 BEGIN
-    return (select isnull(max(precio), 0) from habitaciones 
-    where idhabitacion = @habitacion 
-    and idalojamiento = @alojamiento)
+if exists (select idhabitacion from habitaciones 
+    where idalojamiento = @idalojamiento
+    and precio < @precio) return 1
+    ELSE return 0
 END
     
 create trigger tr_checkPrecio
@@ -77,16 +78,46 @@ after update
 as
 BEGIN
 
-declare @precio decimal(5,2), @alojamiento int
+  declare @precio decimal(5,2), @alojamiento int
 
 select
-    @precio = precio
+    @precio = precio,
+
+    @habitacion = (select idhabitacion from habitaciones
+    where idhabitacion = (select idhabitacion from inserted)),
+    
+    @alojamiento = (select idalojamiento from habitaciones ha
+    inner join inserted i on i.idalojamiento = ha.idalojamiento)
     from inserted
 
-
-
+IF ((select baño from habitaciones where idhabitacion = @idhabitacion) = 'n') 
+    begin
+    
+if (dbo.fn_comparePrecio(@alojamiento, @precio) = 0 and)
+    BEGIN
+      print 'Datos correctos.'
+    END
+else
+    BEGIN
+      print 'No puede existir una habitación sin baño con precio superior a una con baño.'
+      ROLLBACK
+    END
+    
+    end
 END
 
+create procedure pa_checkNivelActividad
+as
+BEGIN
+
+update habitaciones
+set precio = precio + 5
+where baño = 'n' and
+(select ac.dificultad from actividades ac
+inner join alojamiento al on al.idalojamiento = ac.idalojamiento) > 4
+
+  drop procedure pa_checkNivelActividad
+END
 
 
 
